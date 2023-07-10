2f4ddc9d615abca60d404ab7c6eff52be29c0e64
CarleeLaws <carleelaws@developersinstitute.co.nz>
2023-07-10 14:28:24 +1200

    list rendering

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      image: "./assets/images/socks_green.jpg",
+      image: "./assets/images/socks_blue.jpg",
       url: "https://www.vuemastery.com",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
     };
   },
 });
